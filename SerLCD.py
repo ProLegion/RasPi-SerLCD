e1f871b5f8dafb233757b3b94e53a3b92ab3d51d
OwenIsLegion <ProIVXX@gmail.com>
2021-02-25 14:59:59 -0500

    Main Python File

---
+++ SerLCD.py
@@ -0,0 +1,24 @@
+#import the Circuit Python board and busio libraries
+import board
+import busio
+
+#enable SPI communications
+import digitalio
+from sparkfun_serlcd import Sparkfun_SerLCD_SPI
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+
+#Setup chip select, CE0
+cs = digitalio.DigitalInOut(board.CE0)
+cs.direction = digitalio.Direction.OUTPUT
+
+serlcd = Sparkfun_SerLCD_SPI(spi, cs)
+
+#Initalization Done
+
+#Clear Display
+serlcd.clear()
+
+#Set Backlight RGB to Cyan
+serlcd.set_backlight_rgb(0,245,100)
+serlcd.write("LCD working with\nthe pi")
